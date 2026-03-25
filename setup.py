200b2d9d4910816beb194a468b6864eff14b297b
Juan Moises de la Serna <jmsernatuya@gmail.com>
2026-03-25 09:10:24 +0100

    Add/update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup, find_packages
-setup(
-    name="impacto-ia-mercado-laboral-america-latina-2020-2025",
-    version="1.0.0",
-    description="Impacto de la Inteligencia Artificial en el Mercado Laboral de América Latina (2020-2025)",
-    author="de la Serna, Juan Moisés",
-    url="https://github.com/juanmoisesd/impacto-ia-mercado-laboral-america-latina-2020-2025",
-    packages=find_packages(),
-    install_requires=["pandas>=1.3.0","requests>=2.26.0"],
-    python_requires=">=3.7",
-    classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Topic :: Scientific/Engineering"],
-    keywords="zenodo, open-data",
-)
+from setuptools import setup,find_packages
+setup(name="impacto-ia-mercado-laboral-america-latina-2020-2025",version="1.0.0",description="Impacto de la Inteligencia Artificial en el Mercado Laboral de América Latina (2020-2025)",author="de la Serna, Juan Moisés",url="https://github.com/juanmoisesd/impacto-ia-mercado-laboral-america-latina-2020-2025",packages=find_packages(),install_requires=["pandas>=1.3.0","requests>=2.26.0"],python_requires=">=3.7",classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Topic :: Scientific/Engineering"],keywords="zenodo, open-data, dataset")
